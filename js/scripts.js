75fd4ed92aafb51c954b60209257a002d2e803a2
Marta Sanchez <sanchez.marta@gmail.com>
2016-08-15 01:02:19 -0700

    change to a new story and design

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var person3Input = $("input#person3").val();
+    var adjective1 = $("input#adjective1").val();
+    var name = $("input#name").val();
+    var singularNoun = $("input#singular-noun").val();
+    var largeNumber = $("input#large-number").val();
+    var famousPerson = $("input#famous-person").val();
+    var adjective2 = $("input#adjective2").val();
+    var number = $("input#number").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".person3").text(person3Input);
+
+    $(".adjective1").text(adjective1);
+    $(".name").text(name);
+    $(".singular-noun").text(singularNoun);
+    $(".large-number").text(largeNumber);
+    $(".famous-person").text(famousPerson);
+    $(".adjective2").text(adjective2);
+    $(".number").text(number);
 
     $("#story").show();
+    $("#blanks").hide();
+    $("#form-header").hide();
+    document.body.style.backgroundImage = "url(img/book-background.jpg)";
 
     event.preventDefault();
   });
