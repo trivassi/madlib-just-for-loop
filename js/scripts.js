349b8a3705032b1c065963afba6b00a5368f47c7
David Ayala and Marta Sánchez <david@>
2016-08-15 10:54:57 -0700

    DRY the javascript code using forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var adjective1 = $("input#adjective1").val();
-    var name = $("input#name").val();
-    var singularNoun = $("input#singular-noun").val();
-    var largeNumber = $("input#large-number").val();
-    var famousPerson = $("input#famous-person").val();
-    var adjective2 = $("input#adjective2").val();
-    var number = $("input#number").val();
 
+    var blanks = ["adjective1", "name", "singular-noun", "large-number", "famous-person", "adjective2", "number"];
 
-    $(".adjective1").text(adjective1);
-    $(".name").text(name);
-    $(".singular-noun").text(singularNoun);
-    $(".large-number").text(largeNumber);
-    $(".famous-person").text(famousPerson);
-    $(".adjective2").text(adjective2);
-    $(".number").text(number);
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
     $("#blanks").hide();
