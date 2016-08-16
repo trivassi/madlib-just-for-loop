77defa711599db13e7f7f0969074c518f8ea1b12
Marta Sanchez and Thomas Rivas <thomas@>
2016-08-16 09:56:13 -0700

    will update the foreach loop to a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
 
     var blanks = ["adjective1", "name", "singular-noun", "large-number", "famous-person", "adjective2", "number"];
 
-    blanks.forEach(function(blank){
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
-    });
+    // blanks.forEach(function(blank){
+    //   var userInput = $("input#" + blank).val();
+    //   $("." + blank).text(userInput);
+    // });
+
+    for (var i = 0; i < blanks.length; i++) {
+      var userInput = $("input#" + blanks[i]).val();
+      $("." + blanks[i]).text(userInput);
+    }
 
     $("#story").show();
     $("#blanks").hide();
